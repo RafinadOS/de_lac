84e9e6f3de13aca63ccd1222b621b8914e0e9537
Pavel Rafeev <rafeev.pavel@gmail.com>
2016-01-25 15:01:24 +0300

    cart complete

--- js/main.js
+++ js/main.js
@@ -4,8 +4,23 @@ $(document).ready(function($) {
 		$('.overlay').toggleClass('overlay-show');
 		return false
 	});
-	$('.overlay').click(function(event) {
+	$('.overlay').click(function(e) {
 		$(this).toggleClass('overlay-show');
 		$('footer').toggleClass('show');
 	});
+
+	// вызов корзины
+	$('.cart-popup-close').click(function(e) {
+		$('.cart-popup').toggleClass('cart-popup-show');
+		return false
+	});
+	$('.purchases__link').click(function(e) {
+		$('.cart-popup').toggleClass('cart-popup-show');
+		return false
+	});
+
+	// инициализация кастомного скрола
+	$('.ship-list').jScrollPane();
 });
+
+
