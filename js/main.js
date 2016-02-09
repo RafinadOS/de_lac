356a3cfdee756dcdd4775290bb55bc30e4f8e4d6
Pavel Rafeev <rafeev.pavel@gmail.com>
2016-02-09 18:59:10 +0300

    add responsiv

--- js/main.js
+++ js/main.js
@@ -4,23 +4,38 @@ $(document).ready(function($) {
 		$('.overlay').toggleClass('overlay-show');
 		return false
 	});
-	$('.overlay').click(function(e) {
+	$('.overlay').click(function() {
 		$(this).toggleClass('overlay-show');
-		$('footer').toggleClass('show');
+		$('footer').removeClass('show');
+		$('body').removeClass('header__menu-vissible');
 	});
 
 	// вызов корзины
-	$('.cart-popup-close').click(function(e) {
-		$('.cart-popup').toggleClass('cart-popup-show');
+	$('.cart-popup-close').click(function() {
+		$('body').toggleClass('cart-popup-show');
 		return false
 	});
-	$('.purchases__link').click(function(e) {
-		$('.cart-popup').toggleClass('cart-popup-show');
+	$('.purchases__link').click(function() {
+		$('body').toggleClass('cart-popup-show');
+		return false
+	});
+
+	$('.header__menu-show').click(function() {
+		$('body').toggleClass('header__menu-vissible');
+		$('.overlay').toggleClass('overlay-show');
 		return false
 	});
 
 	// инициализация кастомного скрола
 	$('.ship-list').jScrollPane();
+
+	$(window).load(function() {
+		setTimeout(function() {
+			$('#loader-wrapper').fadeOut('slow', function() {});
+		}, 500);
+	});
 });
+
+
 
 
